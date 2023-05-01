32857eb60d10b8d8c14f632ffdf02e7960c1cb8e
chenxutian <101193435+Blossom0913@users.noreply.github.com>
2023-05-01 05:36:07 +0000

    0501

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,7 +6,7 @@
 // The program should wait until all the spawned threads have finished and
 // should collect their return values into a vector.
 
-// I AM NOT DONE
+
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -25,7 +25,9 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
-    }
+        handle.join(); // Yes, you can.
+        results.push(1);
+        }
 
     if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
